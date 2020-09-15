c1ae196d5850b235484392a00c947d9190dcfda4
CrisRuedaP <crisruedap@gmail.com>
2020-09-15 02:01:30 +0000

    Fixed the output

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,22 +9,21 @@ if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com'
     employees = requests.get(url + '/users/').json()
 
+    data = {}
+    all_tasks = []
     for user in employees:
         user_id = user.get('id')
         username = user.get('username')
         amount = requests.get(
             url + '/todos?userId={}'.format(user_id)).json()
+        for task in amount:
+            records = {}
+            records["username"] = username
+            records["task"] = task.get("title")
+            records["completed"] = task.get("completed")
+            all_tasks.append(records)
+        data[user_id] = all_tasks
 
-    all_tasks = []
-    for task in amount:
-        records = {}
-        records["username"] = username
-        records["task"] = task.get("title")
-        records["completed"] = task.get("completed")
-        all_tasks.append(records)
-
-    data = {}
-    data[user_id] = all_tasks
     filename = 'todo_all_employees.json'
     with open(filename, 'w') as file:
         json.dump(data, file)
